78cfb14166095f8d70580d125b55039bcbe2a97e
Alex Antonio Soare <116096714+raizojpg@users.noreply.github.com>
2025-01-06 13:26:58 +0200

    Update scheduler.c logging

--- scheduler.c
+++ scheduler.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <time.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <signal.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "scheduler.h"
 
@@ -19,15 +22,40 @@
 #define TIME_QUANTUM 3
 
 struct CPU cpus[CNT_CPUS];
-
+int logging;
 
 static inline float min(float a, float b) {
     return (a < b) ? a : b;
 }
 
+void print_in_file(const char *format, ...) {
+    char buffer[4096];  
+    va_list args;
+    
+    va_start(args, format);
+    vsnprintf(buffer, sizeof(buffer), format, args); 
+    va_end(args);
+
+    write(logging, buffer, strlen(buffer));
+}
+
+void print_users(struct CPU* cpu){
+  print_in_file("\n");
+  struct User* ptr = cpu->current;
+    if(ptr != NULL){
+      int start = ptr->uid;
+      do{
+        print_in_file("User ID %d has allocated time %.2f and %d processes\n", ptr->uid, ptr->allocated_time_value, ptr->cnt_processes_incoming);
+        ptr=ptr->next;
+        if(ptr==NULL){break;}
+      }while(ptr->uid != start);
+    }
+    print_in_file("\n");
+}
+
 void print_processes(struct User* usr){
-    printf("Incoming : ");
-    struct Process* ptr = usr->current_incoming;
+    printf("Available: ");
+    struct Process* ptr = usr->current_available;
     if(ptr != NULL){
       int start = ptr->pid;
       do{
@@ -37,8 +65,8 @@ void print_processes(struct User* usr){
       }while(ptr->pid != start);
     }
     printf("\n");
-    printf("Available: ");
-    ptr = usr->current_available;
+    printf("Incoming : ");
+    ptr = usr->current_incoming;
     if(ptr != NULL){
       int start = ptr->pid;
       do{
@@ -66,10 +94,10 @@ struct Process* generate_processes(struct User* usr)
 
     if (pid == 0)
     {
-      printf("User ID: %d with process %d and arrival time %f\n", usr->uid, getpid(), proc->arrival_time);
+      print_in_file("Process %d ( user %d ): arrival time %.2f and execution time %.2f\n", getpid(), usr->uid, proc->arrival_time, proc->exec_time);
       kill(getpid(), SIGSTOP);
       while(true){
-        printf("----Process %d running on CPU...\n", getpid());
+        printf("-----> Process %d running on CPU...\n", getpid());
         sleep(3);
       }
   
@@ -107,14 +135,12 @@ struct User* generate_users(struct CPU* cpu)
     struct User* usr = malloc(sizeof(struct User));
     usr->uid = i;
     // user->username = "user" + i;
-    usr->weight = (rand() % SPAN_USER_WEIGHT) / (float)SPAN_USER_WEIGHT;
+    usr->weight = ((rand() % SPAN_USER_WEIGHT) + 10 ) / (float)SPAN_USER_WEIGHT;
     usr->allocated_time_value = BASE_USER_TIME * usr->weight;
     usr->total_user_time = 0;
-    printf("User ID: %d has allocated time %.5f \n", usr->uid, usr->allocated_time_value);
-
+    
     usr->cnt_processes_available = 0;
     usr->cnt_processes_incoming = rand() % SPAN_CNT_PROCESSES + 1;
-    //usr->cnt_processes_incoming =  3;
 
     usr->current_available = NULL;
     usr->current_incoming = generate_processes(usr);
@@ -132,17 +158,15 @@ struct User* generate_users(struct CPU* cpu)
   return head;
 }
 
-
 float update_available_processes(struct User* usr)
 {
-  //printf("User time %f\n",usr->total_user_time ); 
   if(usr->current_incoming != NULL){
 
     struct Process* min_arrival_proc = usr->current_incoming;
 
     for (int i = 0; i < usr->cnt_processes_incoming; i++)
     {
-      printf("Process %d arival time %f and exec time %f\n",usr->current_incoming->pid,usr->current_incoming->arrival_time,usr->current_incoming->exec_time);
+      printf("Process %d arival time %.2f and exec time %.2f\n",usr->current_incoming->pid,usr->current_incoming->arrival_time,usr->current_incoming->exec_time);
     
       if (usr->current_incoming->arrival_time < min_arrival_proc->arrival_time)
       {
@@ -192,14 +216,12 @@ float update_available_processes(struct User* usr)
             aux->prev = available;
           }
         }
-        
       } 
       else
       { 
         usr->current_incoming = usr->current_incoming->next;
       }
     }
-    
     if (usr->current_available == NULL)
     {
       return min_arrival_proc->arrival_time;
@@ -211,15 +233,13 @@ float update_available_processes(struct User* usr)
 float rr_processes_scheduler(struct User* usr)
 {
   float retval = update_available_processes(usr);
-  printf("RR: available %d and incoming %d\n",usr->cnt_processes_available, usr->cnt_processes_incoming);
+  printf("RR Processes: %d available and %d incoming\n",usr->cnt_processes_available, usr->cnt_processes_incoming);
   print_processes(usr);
-
-  //sleep(2);
 
   if (retval == 0)
   {
     struct Process* proc = usr->current_available;
-    printf("Procesul disponibil acum este %d cu %f exec time\n", proc->pid,proc->exec_time);
+    //printf("Procesul %d disponibil -> %.2f exec time\n", proc->pid,proc->exec_time);
 
     float available_user_time = usr->allocated_time;
     float time_quantum = TIME_QUANTUM;
@@ -229,7 +249,7 @@ float rr_processes_scheduler(struct User* usr)
       float available_exec_time = min(available_user_time,time_quantum);
       float actual_exec_time = min(available_exec_time,proc->exec_time);
       
-      printf("Pornim executia procesului cu exec time %f\n", proc->exec_time);
+      printf("Start execution -> exec time %.2f\n", proc->exec_time);
     
       kill(proc->pid, SIGCONT);
       sleep(actual_exec_time);
@@ -238,13 +258,13 @@ float rr_processes_scheduler(struct User* usr)
       proc->exec_time-=actual_exec_time;
       available_user_time-=actual_exec_time;
       
-      printf("Oprim executia procesului cu exec time %f\n", proc->exec_time);
-      //printf("S-a executat %f\n",actual_exec_time);
+      printf("Stop execution -> exec time %.2f\n", proc->exec_time);
       
       if (proc->exec_time <= 0.001) {
-        printf("\nSUCCESS: Process %d has finished execution.\n\n", proc->pid);
-        kill(proc->pid, SIGKILL);
+        printf("\nSUCCESS: Process %d (%d) has finished execution.\n\n", proc->pid, proc->uid);
+        print_in_file("SUCCESS: Process %d (%d) has finished execution.\n", proc->pid, proc->uid);
 
+        kill(proc->pid, SIGKILL);
         // Wait for the process to fully terminate
         waitpid(proc->pid, NULL, 0);
 
@@ -272,7 +292,7 @@ float rr_processes_scheduler(struct User* usr)
   {
     printf("No process available now\n");
     float waited = min(retval, usr->allocated_time);
-    printf("Retval %f, Waited %f\n",retval,waited);
+    printf("Retval %.2f, Waited %.2f\n",retval,waited);
     //sleep(waited);
     usr->allocated_time -= waited;
     if (usr->allocated_time > 0)
@@ -290,15 +310,19 @@ float wrr_users_scheduler(struct CPU* cpu)
   while (usr != NULL)
   {
     usr->allocated_time = usr->allocated_time_value;
-    printf("\nWRR before: Allocated time %f and total time %f for USER %d\n", usr->allocated_time, usr->total_user_time, usr->uid);
+    printf("\n----------------------------------------------\n");
+    printf("WRR Users: Allocated time %.2f and total time %.2f for USER %d\n", usr->allocated_time, usr->total_user_time, usr->uid);
     usr->total_user_time += rr_processes_scheduler(usr);
-    printf("WRR  after: Allocated time %f and total time %f for USER %d\n", usr->allocated_time, usr->total_user_time, usr->uid);
-     
+    printf("WRR Users: Allocated time %.2f and total time %.2f for USER %d\n", usr->allocated_time, usr->total_user_time, usr->uid);
+    printf("----------------------------------------------\n\n");
+
     if (usr->cnt_processes_incoming == 0 && usr->cnt_processes_available == 0)
     {
-
-      printf("\nSUCCESS: User ID %d finished and had a total time on CPU of %.5f \n\n", usr->uid, usr->total_user_time);  
       cpu->total_runtime += usr->total_user_time;
+
+      printf("\nSUCCESS: User ID %d finished and had a total time on CPU of %.2f \n\n", usr->uid, usr->total_user_time);  
+      print_in_file("\nSUCCESS: User ID %d finished and had a total time on CPU of %.2f \n", usr->uid, usr->total_user_time);  
+      print_in_file("CPU time is %.2f\n\n",cpu->total_runtime);
 
       struct User* aux = usr;
       if(cpu->cnt_users>1)
@@ -319,23 +343,34 @@ float wrr_users_scheduler(struct CPU* cpu)
     }
   }
 
-  printf("Timpul pentrectut pe CPU %f\n",cpu->total_runtime);
-  printf("Bye\n");
+  print_in_file("Total CPU time %.2f\n",cpu->total_runtime);
+  printf("Total CPU time %.2f\n",cpu->total_runtime);
 }
 
 int main() 
 {
   srand(time(NULL));
 
+  logging = open("./logging.txt",O_WRONLY | O_CREAT | O_TRUNC, 0644);
+  if(logging<0){
+    char msg[50] = "Can't open destination file\n";
+    write(2,msg,sizeof(msg));
+    return 1 ;
+  }
+
+  printf("Starting..\n");
   for (int i = 0; i < CNT_CPUS; i++)
   {
     cpus[i].cpu_id = i;
     cpus[i].cnt_users = rand() % SPAN_CNT_USERS + 1;
-    //cpus[i].cnt_users = 2;
     cpus[i].current = generate_users(&cpus[i]);
   }
   
+  sleep(5);
+  print_users(&cpus[0]);
+
   wrr_users_scheduler(&cpus[0]);
-  
+
+  close(logging);
   return 0;
 }
